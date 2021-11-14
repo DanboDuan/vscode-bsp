3cd293aeac8c3a538f98b75c86fc6e295f404ab3
bob <bob170731@gmail.com>
2021-11-14 16:32:40 +0800

    fix export type

---
+++ src/common/internal.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+export {
+    DocumentUri,
+    URI,
+    integer,
+    uinteger,
+    decimal,
+    TextDocumentIdentifier,
+    Diagnostic,
+    Location
+} from 'vscode-languageserver-types'
+
+export {
+    Message,
+    NotificationMessage,
+    CancellationToken,
+    RequestHandler0,
+    RequestHandler,
+    GenericRequestHandler,
+    NotificationHandler0,
+    NotificationHandler,
+    GenericNotificationHandler,
+    ProgressType,
+    Trace,
+    Tracer,
+    TraceOptions,
+    Disposable,
+    Event,
+    MessageReader,
+    MessageWriter,
+    Logger,
+    ConnectionStrategy,
+    ConnectionOptions,
+    createMessageConnection,
+    RequestType0,
+    RequestType,
+    NotificationType0,
+    NotificationType,
+    MessageSignature,
+    ResponseError,
+    _EM,
+    ParameterStructures,
+} from 'vscode-jsonrpc';
