3e9116e6ed516382d8289b3631577554779427f6
bob <bob170731@gmail.com>
2021-12-02 10:50:57 +0800

    update reload request

--- src/common/request.ts
+++ src/common/request.ts
@@ -125,14 +125,8 @@ export namespace ShutdownRequest {
 
 
 export interface ReloadParams {
-    /** The build target to run. */
-    target?: BuildTargetIdentifier;
-
     /** Kind of data to expect in the data field. If this field is not set, the kind of data is not specified. */
     dataKind?: string;
-
-    /** Language-specific metadata for this execution.
-     * See ScalaMainClass as an example. */
     data?: any;
 }
 
